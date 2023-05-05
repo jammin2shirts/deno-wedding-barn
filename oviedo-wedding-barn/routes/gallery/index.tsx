b038eba4a1cf895a09af75e14b0c2912ee9f2f14
jammin2shirts <teuchertb@gmail.com>
2023-05-04 22:40:01 -0400

    Added header element

--- oviedo-wedding-barn/routes/gallery/index.tsx
+++ oviedo-wedding-barn/routes/gallery/index.tsx
@@ -1,37 +1,24 @@
-import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
 import NavBar from "@/islands/NavBar.tsx";
 import Gallery from "@/islands/Gallery.tsx";
+import { HeadElement } from "@/components/HeadElement.tsx";
 
-export default function Home() {
+export default function Home(ctx: PageProps) {
+  const { url } = ctx;
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/lds4gch.css" async>
-        </link>
-        <title>Oviedo Wedding Barn | Gallery</title>
-        <meta name="title" content="The Wedding Barn - Oviedo, FL" />
-        <meta
-          name="description"
-          content="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
-        north of Orlando, our idyllic setting is a family owned and operated venue that prides itself on creating the perfect
-        event for you. The offer of a beautiful setting, stellar customer service, and an inclusive setting are our promise to
-        you. Originally built in 2023, the Wedding Barn features a fully air conditioned event venue, cocktail patio, custom bar,
-        dressing suites, and a catering prep kitchen. You and your guests will enjoy mingling, and wandering the memorable
-        grounds. As a premier reception venue, The Wedding Barn offers memories to last a life time."
-        />
-        <meta name="og:title" content="The Wedding Barn - Oviedo, FL" />
-        <meta
-          name="og:description"
-          content="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
-        north of Orlando, our idyllic setting is a family owned and operated venue that prides itself on creating the perfect
-        event for you. The offer of a beautiful setting, stellar customer service, and an inclusive setting are our promise to
-        you. Originally built in 2023, the Wedding Barn features a fully air conditioned event venue, cocktail patio, custom bar,
-        dressing suites, and a catering prep kitchen. You and your guests will enjoy mingling, and wandering the memorable
-        grounds. As a premier reception venue, The Wedding Barn offers memories to last a life time."
-        />
-        <meta name="og:image" content="/images/webp/full-barn.webp" />
-      </Head>
-      <NavBar selected={3} />
+      <HeadElement
+        description="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
+    north of Orlando, our idyllic setting is a family owned and operated venue that prides itself on creating the perfect
+    event for you. The offer of a beautiful setting, stellar customer service, and an inclusive setting are our promise to
+    you. Originally built in 2023, the Wedding Barn features a fully air conditioned event venue, cocktail patio, custom bar,
+    dressing suites, and a catering prep kitchen. You and your guests will enjoy mingling, and wandering the memorable
+    grounds. As a premier reception venue, The Wedding Barn offers memories to last a life time."
+        image={url.href + "images/webp/full-barn.webp"}
+        title="The Wedding Barn - Oviedo, FL"
+        url={url}
+      />
+      <NavBar />
       <Gallery />
     </>
   );
--- oviedo-wedding-barn/routes/packages/index.tsx
+++ oviedo-wedding-barn/routes/packages/index.tsx
@@ -1,36 +1,23 @@
-import { Head } from "$fresh/runtime.ts";
+import { PageProps } from "$fresh/server.ts";
 import NavBar from "@/islands/NavBar.tsx";
+import { HeadElement } from "@/components/HeadElement.tsx";
 
-export default function Home() {
+export default function Home(ctx: PageProps) {
+  const { url } = ctx;
   return (
     <>
-      <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/lds4gch.css" async>
-        </link>
-        <title>Oviedo Wedding Barn | Packages</title>
-        <meta name="title" content="The Wedding Barn - Oviedo, FL" />
-        <meta
-          name="description"
-          content="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
+      <HeadElement
+        description="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
         north of Orlando, our idyllic setting is a family owned and operated venue that prides itself on creating the perfect
         event for you. The offer of a beautiful setting, stellar customer service, and an inclusive setting are our promise to
         you. Originally built in 2023, the Wedding Barn features a fully air conditioned event venue, cocktail patio, custom bar,
         dressing suites, and a catering prep kitchen. You and your guests will enjoy mingling, and wandering the memorable
         grounds. As a premier reception venue, The Wedding Barn offers memories to last a life time."
-        />
-        <meta name="og:title" content="The Wedding Barn - Oviedo, FL" />
-        <meta
-          name="og:description"
-          content="The Wedding Barn operates a beautiful event space right in the heart of Central Florida. Located just 20 min
-        north of Orlando, our idyllic setting is a family owned and operated venue that prides itself on creating the perfect
-        event for you. The offer of a beautiful setting, stellar customer service, and an inclusive setting are our promise to
-        you. Originally built in 2023, the Wedding Barn features a fully air conditioned event venue, cocktail patio, custom bar,
-        dressing suites, and a catering prep kitchen. You and your guests will enjoy mingling, and wandering the memorable
-        grounds. As a premier reception venue, The Wedding Barn offers memories to last a life time."
-        />
-        <meta name="og:image" content="/images/webp/full-barn.webp" />
-      </Head>
-      <NavBar selected={2} />
+        image={url.href + "images/webp/full-barn.webp"}
+        title="The Wedding Barn - Oviedo, FL"
+        url={url}
+      />
+      <NavBar />
       <div class="p-4 mx-auto max-w-screen-md  font-canada-type-gibson font-extralight">
         <div class="text-2xl">
           What's Included
