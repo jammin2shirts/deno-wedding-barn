fc072cf59725de3746e4836a09f0a8f76c89ab14
jammin2shirts <teuchertb@gmail.com>
2023-05-05 10:25:58 -0400

    fixed site map

--- oviedo-wedding-barn/routes/sitemap.xml.ts
+++ oviedo-wedding-barn/routes/sitemap.xml.ts
@@ -4,7 +4,7 @@ import { SitemapContext } from "https://deno.land/x/fresh_seo@0.2.1/mod.ts";
 export const handler: Handlers = {
   GET() {
     const sitemap = new SitemapContext(
-      "https://oviedo-wedding-barn.deno.dev",
+      "https://www.oviedoweddingbarn.com",
       manifest,
     );
     // You can add additional page here
