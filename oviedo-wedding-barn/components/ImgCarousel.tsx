c2c88452f78a41fde316150f9776f3289d79875d
jammin2shirts <teuchertb@gmail.com>
2023-06-19 22:26:36 -0400

    fixed gallery

---
+++ oviedo-wedding-barn/components/ImgCarousel.tsx
@@ -0,0 +1,38 @@
+import { useState } from "preact/hooks";
+import { Signal } from "@preact/signals";
+import IconCircleChevronsRight from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-right.tsx";
+import IconCircleChevronsLeft from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-left.tsx";
+
+export function ImgCarousel(
+  props: {
+    slides: { color: string; url: string; text: string };
+    currentSelected: Signal<number>;
+  },
+) {
+  const {slides, currentSelected} = props
+  const nextSlide = (direction: number) => {
+    if (currentSelected.value === 0 && direction === -1) {
+      currentSelected.value = slides.length - 1;
+    } else if (currentSelected.value === slides.length - 1 && direction === 1) {
+      currentSelected.value = 0;
+    } else {
+      currentSelected.value += direction;
+    }
+  };
+  return (
+    <div className="md:p-5 flex justify-between items-center w-full h-96 mb-10 ">
+      <IconCircleChevronsLeft
+        class={` w-10 h-10 text-black cursor-pointer`}
+        onClick={() => nextSlide(-1)}
+      />
+      <img
+        src={slides[currentSelected.value].url}
+        class={`max-w-sm md:max-w-lg object-scale-down`}
+      />
+      <IconCircleChevronsRight
+        class={` w-10 h-10 text-black cursor-pointer `}
+        onClick={() => nextSlide(1)}
+      />
+    </div>
+  );
+}
--- oviedo-wedding-barn/islands/Gallery.tsx
+++ oviedo-wedding-barn/islands/Gallery.tsx
@@ -1,61 +1,83 @@
 import { JSX } from "preact";
 import { useState } from "preact/hooks";
 import { IS_BROWSER } from "$fresh/runtime.ts";
+import { useSignal } from "@preact/signals";
+import { useEffect, useRef } from "preact/hooks";
+import IconCircleChevronsRight from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-right.tsx";
+import IconCircleChevronsLeft from "https://deno.land/x/tabler_icons_tsx@0.0.3/tsx/circle-chevrons-left.tsx";
+import { ImgCarousel } from "@/components/ImgCarousel.tsx";
+
+const SLIDE_DATA = [
+  {
+    color: "",
+    text: "slide five",
+    url: "/images/webp/barn-door-view.webp",
+  },
+  {
+    color: "",
+    text: "slide one",
+    url: "/images/webp/ceremony.webp",
+  },
+  {
+    color: "",
+    text: "slide four",
+    url: "/images/webp/patio.webp",
+  },
+  {
+    color: "",
+    text: "slide four",
+    url: "/images/webp/zoomed-in-rafter.webp",
+  },
+  {
+    color: "",
+    text: "slide three",
+    url: "/images/webp/interior.webp",
+  },
+  {
+    color: "",
+    text: "slide two",
+    url: "/images/webp/full-barn.webp",
+  },
+];
 
 export default function Gallery(props: JSX.HTMLAttributes) {
-  const [isSelected, setIsSelected] = useState();
+  let shownImgIndex = useSignal<number>(0);
+  let clone: Array<any> = structuredClone(SLIDE_DATA);
+  let chunks = [];
+  const chunkSize = Math.floor(SLIDE_DATA.length / 3);
+  console.log(chunkSize);
+  while (clone.length > 0) {
+    chunks.push(clone.splice(0, chunkSize));
+  }
   return (
     <div class="mx-auto max-w-screen-lg font-canada-type-gibson">
-      
-      <div class="container mx-auto border-2 rounded-lg  px-5 py-2 ">
-        <div class="-m-1 flex flex-wrap md:-m-2">
-          <div class="flex w-1/2 flex-wrap">
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/ceremony.webp"
-              />
+      <ImgCarousel slides={SLIDE_DATA} currentSelected={shownImgIndex} />
+
+      <div
+        class={`mx-auto grid md:grid-cols-3 gap-5`}
+      >
+        {chunks.map((chunk, chunkIndex) => {
+          return (
+            <div class="flex flex-col gap-5">
+              {chunk.map((image, index) => {
+                return (
+                  <div>
+                    <img
+                      class="h-auto max-w-full rounded-lg"
+                      src={image.url}
+                      alt={image.text}
+                      onClick={() => {
+                        shownImgIndex.value = (chunkIndex === 0)
+                          ? index
+                          : (chunkIndex * chunkSize) + index;
+                      }}
+                    />
+                  </div>
+                );
+              })}
             </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/patio.webp"
-              />
-            </div>
-            <div class="w-full p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/barn-door-view.webp"
-              />
-            </div>
-          </div>
-          <div class="flex w-1/2 flex-wrap">
-            <div class="w-full p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/zoomed-in-rafter.webp"
-              />
-            </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/interior.webp"
-              />
-            </div>
-            <div class="w-1/2 p-1 md:p-2">
-              <img
-                alt="gallery"
-                class="block h-full w-full rounded-lg object-cover object-center"
-                src="images/webp/family.webp"
-              />
-            </div>
-          </div>
-        </div>
+          );
+        })}
       </div>
     </div>
   );
