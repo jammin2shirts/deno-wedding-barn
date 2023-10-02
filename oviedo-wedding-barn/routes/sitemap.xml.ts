bb4668837a89772f036d7430259551f84fa2c802
jammin2shirts <teuchertb@gmail.com>
2023-10-02 13:30:20 -0400

    Update sitemap

--- oviedo-wedding-barn/routes/sitemap.xml.ts
+++ oviedo-wedding-barn/routes/sitemap.xml.ts
@@ -2,11 +2,8 @@ import { Handlers } from "$fresh/server.ts";
 import manifest from "../fresh.gen.ts";
 import { SitemapContext } from "https://deno.land/x/fresh_seo@0.2.1/mod.ts";
 export const handler: Handlers = {
-  GET() {
-    const sitemap = new SitemapContext(
-      "https://www.oviedoweddingbarn.com",
-      manifest,
-    );
+  GET(req, ctx) {
+    const sitemap = new SitemapContext("https://www.oviedoweddingbarn.com", manifest);
     // You can add additional page here
     return sitemap.render();
   },
